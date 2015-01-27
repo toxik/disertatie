bf5e54951f8c579b343317add265e67c4b72448a
Alexandru Georoceanu <alex@navigheaza.ro>
2015-01-28 00:03:43 +0200

    Improved the start-stop scripts; full redoing of ip list

---
+++ src/updatelist.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+PORT=3000
+echo "upstream io_nodes { ip_hash;" > ../nginx/conf.d/_io-nodes.conf
+docker ps -a | grep "node " | awk '{print $1}' | while read cont; do echo "server $(docker inspect -f '{{.NetworkSettings.IPAddress}}' $cont):$PORT;" >> ../nginx/conf.d/_io-nodes.conf ; done
+echo } >> ../nginx/conf.d/_io-nodes.conf
+docker exec -it proxy nginx -s reload
