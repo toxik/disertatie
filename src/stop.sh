ce1870ec5c10c3f01ef9c94090f6483624e6e914
Alexandru Georoceanu <alex@navigheaza.ro>
2015-01-28 08:36:49 +0200

    Pimped stop script to stop all containers if no args given

--- src/stop.sh
+++ src/stop.sh
@@ -1,7 +1,13 @@
+#!/bin/bash
 #myip=$(docker inspect -f '{{.NetworkSettings.IPAddress}}' $1)
 #docker exec -ti redis redis-cli lrem frontend:docker -1 "http://$myip:3000"
 #sed "s/server $myip:3000;//g" ../nginx/conf.d/_io-nodes.conf | sed /^$/d > tmp && mv tmp ../nginx/conf.d/_io-nodes.conf
 #docker exec -it proxy nginx -s reload
-docker rm -f $1
+if [ -z $1  ]
+then
+containers="$(docker ps -a | grep "node " | awk '{print $1}')"
+else
+containers=$1
+fi
+echo "$containers" | while read x; do docker rm -f $x; done
 ./updatelist.sh
-rm -f $1
